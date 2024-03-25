10010251581df3b92618d93211547dec2c2a16fe
subodh r pataki <subodh.r.pataki@gamil.com>
2024-03-25 13:30:09 +0530

    cleaned code, added fully functional mobile nav bar

--- src/components/hamburger.js
+++ src/components/hamburger.js
@@ -1,9 +1,16 @@
 window.onload = function () {
     const menu_btn = document.querySelector('.hamburger');
     const mobile_menu = document.querySelector('.mobile-nav');
+    const menuLinks = document.querySelector('.mobile-nav-a');
 
     menu_btn.addEventListener('click', function () {
-        menu_btn.classList.toggle('is-active');
-        mobile_menu.classList.toggle('is-active');
+        menu_btn.classList.toggle('is-active');             // cross hamburger
+        mobile_menu.classList.toggle('is-active');          // open mobile nav
     });
+
+    menuLinks.addEventListener('click', function () {
+        menu_btn.classList.remove('is-active');             // bar hamburger
+        mobile_menu.classList.remove('is-active');          // close mobile nav
+    });
+
 }
